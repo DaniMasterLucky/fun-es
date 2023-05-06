41f51952342ba6114c843eac0343845db15b8f5f
Aluno talentos <raul.hiago.salomao@gmail.com>
2023-05-05 21:17:02 -0300

    função

---
+++ intex.js
@@ -0,0 +1,37 @@
+// const nome = prompt("Digite seu nome")
+// const idade = Number(prompt("Digite sua idade")) 
+// const cidade = prompt("digite sua cidade")
+// const profissão = prompt("Digite sua profissão")
+
+// function listarPessoal(NomePessoa,IdadePessoa,cidadePessoa,profissãoPessoa){
+//     console.log(`Eu sou ${NomePessoa}, tenho ${IdadePessoa} anos, moro em ${cidadePessoa} e sou ${profissãoPessoa}.`)
+// };
+
+
+// listarPessoal(nome,idade,cidade,profissão)
+
+
+// function somar(n1, n2) {
+//     return n2 + n2
+
+
+// }
+
+// function MaiorQue(n1, n2) {
+//     return n1 >= n2
+// }
+
+
+// function ehPar(n1, n2) {
+//     return n1 % n2 === 0;
+
+// }
+
+
+// console.log(somar(10, 20));
+// console.log(MaiorQue(10, 20));
+// console.log(ehPar(10, 20));
+
+
+
+
